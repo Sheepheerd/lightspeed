9c50d09c20963760c2955f7076980bc0239d993d
Sheepheerd <130428152+Sheepheerd@users.noreply.github.com>
2025-05-03 01:53:12 -0400

    Start up on gtk is slow

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use mouce::common::MouseButton;
-use mouce::{Mouse, MouseActions};
-use std::thread;
-use std::time::Duration;
-
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, EventControllerKey};
+use mouce::{Mouse, MouseActions};
 
 fn main() {
-    let app = Application::builder()
-        .application_id("org.example.HelloWorld")
-        .build();
+    let app = Application::builder().application_id("lightspeed").build();
 
     app.connect_activate(|app| {
-        let window = ApplicationWindow::builder().application(app).build();
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(1920)
+            .default_height(1080)
+            .title("lightspeed")
+            .build();
 
         let event_controller = EventControllerKey::new();
 
@@ -24,15 +23,18 @@ fn main() {
         });
         window.add_controller(event_controller);
         window.set_resizable(false);
+        // window.maximize();
+        window.set_decorated(false);
+
         window.present();
         window.set_opacity(0.2f64);
     });
 
     app.run();
 
-    let mouse_manager = Mouse::new();
-    let _ = mouse_manager.move_to(1000, 1000);
-
-    thread::sleep(Duration::from_millis(2));
-    let _ = mouse_manager.click_button(&MouseButton::Left);
+    // let mouse_manager = Mouse::new();
+    // let _ = mouse_manager.move_to(1000, 1000);
+    //
+    // thread::sleep(Duration::from_millis(2));
+    // let _ = mouse_manager.click_button(&MouseButton::Left);
 }
